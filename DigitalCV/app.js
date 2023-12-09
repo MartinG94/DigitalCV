d6e02c0101874e3245594396d93cfffbb3a2eacd
Martin Guillen <guillenmartin94@gmail.com>
2023-12-09 11:15:01 -0300

    Comentarios para separar secciones del código

--- DigitalCV/app.js
+++ DigitalCV/app.js
@@ -1,3 +1,4 @@
+/* IMPORTS */
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,16 +10,18 @@ const loggerMiddleware = require('./middlewares/loggerMiddleware');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+/* CONFIGURACIÓN DEL SERVIDOR */
 var app = express();
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`)
 });
 
-// view engine setup
+/* CONFIGURACIÓN DEL MOTOR DE VISTAS */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+/* MIDDLEWARES */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,9 +29,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(loggerMiddleware);
 
+/* ENRUTAMIENTO */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+/* MANEJO DE ERRORES */
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
