133b713c5ba454859a57cfacee71473869438795
Martin Guillen <guillenmartin94@gmail.com>
2023-12-09 03:40:03 -0300

    Definición de loggerMiddleware

--- DigitalCV/app.js
+++ DigitalCV/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const PORT = process.env.PORT || 3000;
-
+const loggerMiddleware = require('./middlewares/loggerMiddleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -24,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(loggerMiddleware);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ DigitalCV/middlewares/loggerMiddleware.js
@@ -0,0 +1,8 @@
+const loggerMiddleware = (req, res, next) => {
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+    next();
+  };
+
+// NOTA: Debe actualizar un archivo.
+
+module.exports = loggerMiddleware;
