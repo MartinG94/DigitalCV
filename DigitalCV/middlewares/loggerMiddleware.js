d623d3c6c5cc87948af2ba8c19179f1ea1914f4c
Martin Guillen <guillenmartin94@gmail.com>
2023-12-11 17:58:11 -0300

    Cambio del formato fecha

--- DigitalCV/middlewares/loggerMiddleware.js
+++ DigitalCV/middlewares/loggerMiddleware.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
 const loggerMiddleware = (req, res, next) => {
-    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  const currentDate = new Date();
+    const formattedDate = `${currentDate.getDate().toString().padStart(2, '0')}/${(currentDate.getMonth() + 1).toString().padStart(2, '0')}/${currentDate.getFullYear()} - ${currentDate.getHours().toString().padStart(2, '0')}:${currentDate.getMinutes().toString().padStart(2, '0')}:${currentDate.getSeconds().toString().padStart(2, '0')}.${currentDate.getMilliseconds().toString().padStart(3, '0')}`;
+    console.log(`${formattedDate} - ${req.method} ${req.url}`);
     next();
   };
 
